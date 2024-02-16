516d05789412b02d84d8f6993da9d5a79c2ae5b5
Patranto Bagus Prabowo <prabowo@xprenia.com>
2024-02-16 21:59:42 +0700

    adding "Pretest" controller

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,21 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// pretest route
+Route::get('pretest', function() {
+
+    foreach(range(1, 100) as $i) {
+        $d = $i;
+        if ($i % 3 == 0 && $i % 5 == 0) {
+            $d = "TigaLima";
+        } else if ($i % 5 == 0) {
+            $d = "Lima";
+        } else if ($i % 3 == 0) {
+            $d = "Tiga";
+        }
+
+        echo $d. '<br/>';
+    }
+
+});
